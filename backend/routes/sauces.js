8c8ea0650cd6cbd388ccd9c3bbdb5765e0776d1d
Emile Bl <bailay.emile@live.fr>
2020-06-04 21:21:18 +0200

    comments added on routes files

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const saucesCtrl = require('../controllers/sauces');
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const saucesCtrl = require('../controllers/sauces'); // We need our controllers to indicate the actions of each route
+const auth = require('../middleware/auth'); // We need the auth middleware for authentifying every route
+const multer = require('../middleware/multer-config'); // Aswell as multer for adding an image the post and put routes
 
-router.post('/', auth, multer, saucesCtrl.createSauce);
-router.put('/:id', auth, multer, saucesCtrl.modifySauce);
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.get('/', auth, saucesCtrl.getAllSauces);
-router.post('/:id/like', auth, saucesCtrl.reactToSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce); // Sauce creation route, need multer for the images
+router.put('/:id', auth, multer, saucesCtrl.modifySauce); // Route where you can modify your sauce, multer loaded if you need to replace the image
+router.delete('/:id', auth, saucesCtrl.deleteSauce); // Route where you can delete your sauce
+router.get('/:id', auth, saucesCtrl.getOneSauce); // Route loaded after clicking on a specific sauce
+router.get('/', auth, saucesCtrl.getAllSauces); // Main route where all the sauces are loaded
+router.post('/:id/like', auth, saucesCtrl.reactToSauce); // Route where you can like or dislike any of the existing sauces
 
-module.exports = router;
+module.exports = router; // We export the routes so that they can be called in our app.js
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,9 +4,9 @@ const bouncer = require ("express-bouncer")(9000, 600000, 3);
 // the express-bouncer module prevents brute force attacks, it restricts the user to only 3 attempts to login
 // If the user uses these 3 attempts, he will be blocked for a duration between 9000 and 600000 milliseconds
 
-const userCtrl = require('../controllers/user');
+const userCtrl = require('../controllers/user'); // We need our controllers to indicate the actions of each route
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', bouncer.block, userCtrl.login); // Execution of the bouncer module on the login route
+router.post('/signup', userCtrl.signup); // Route to signup on the application
+router.post('/login', bouncer.block, userCtrl.login); // Route to login on the application, with the use of the bouncer module to avoid brute force attack
 
-module.exports = router;
+module.exports = router; // We export the routes so that they can be called in our app.js
